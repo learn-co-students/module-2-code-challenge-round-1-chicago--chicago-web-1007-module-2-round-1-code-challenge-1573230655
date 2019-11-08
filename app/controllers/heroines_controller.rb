40c2d03c7057aba441cb501760b2cd9ddf0c2a27
Valeria <valeria.gutsuliak@gmail.com>
2019-11-08 11:58:36 -0600

    valeriia

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,32 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit, :update, :destroy ]
+  
   def index
     @heroines = Heroine.all
   end
+  def show
+  end 
+  def new
+  end 
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroines_path
+    else 
+      redirect_to new_heroine_path
+    end 
+    def edit
+    end 
+    def update
+    end 
+  end 
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_name)
+  end 
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end 
+ 
 end
--- db/migrate/20170725185425_create_heroines.rb
+++ db/migrate/20170725185425_create_heroines.rb
@@ -1,8 +1,11 @@
 class CreateHeroines < ActiveRecord::Migration[5.0]
   def change
+    add_foreign_key :heroines, :powers
+
     create_table :heroines do |t|
       t.string :name
       t.string :super_name
+      t.integer :power_id
 
       t.timestamps
     end
