1a3bd891c4150377c790d33611aebd57d485ee4a
Daniel Ionita <danielionita@Daniels-MacBook-Pro.local>
2019-11-08 11:15:28 -0600

    finished 1-7

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit, :update, :destroy]
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
 end
